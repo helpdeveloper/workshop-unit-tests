dd7c2b15d39d79856303d2f887d01e2553f33ccb
Guilherme Biff Zarelli <guilherme.zarelli@luizalabs.com>
2021-05-05 14:38:39 -0300

    first test

--- src/main/java/br/com/helpdev/workshopunittest/business/FinanceSimulateBusiness.java
+++ src/main/java/br/com/helpdev/workshopunittest/business/FinanceSimulateBusiness.java
@@ -5,6 +5,7 @@ import br.com.helpdev.workshopunittest.business.model.SimulationData;
 import br.com.helpdev.workshopunittest.business.model.SimulationResult;
 import br.com.helpdev.workshopunittest.utils.DocumentUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,23 +14,28 @@ public class FinanceSimulateBusiness {
   @Autowired
   private SerasaService serasaService;
 
+  @Value("${finance.person.calc.value}")
+  private Long personCalcValue;
+
+  @Value("${finance.company.calc.value}")
+  private Long companyCalcValue;
 
   public SimulationResult simulate(final SimulationData simulationData) {
 
     final var debits = serasaService.getDebits(simulationData.getDocument());
-    if (debits.isEmpty()) {
+    if (!debits.isEmpty()) {
       throw new SecurityException("Your document has debits!");
     }
 
     if (DocumentUtils.isCpf(simulationData.getDocument())) {
 
-      if ((simulationData.getFinancialValue() / simulationData.getIncome()) > 100) {
+      if ((simulationData.getFinancialValue() / simulationData.getIncome()) > personCalcValue) {
         throw new IllegalArgumentException("You can't finance this value for person");
       }
 
     } else if (DocumentUtils.isCnpj(simulationData.getDocument())) {
 
-      if ((simulationData.getFinancialValue() / simulationData.getIncome()) > 20) {
+      if ((simulationData.getFinancialValue() / simulationData.getIncome()) > companyCalcValue) {
         throw new IllegalArgumentException("You can't finance this value for company");
       }
 
@@ -42,5 +48,13 @@ public class FinanceSimulateBusiness {
     var parcelValue = simulationData.getFinancialValue() / simulationData.getParcels();
     parcelValue *= 0.1;
     return new SimulationResult(parcelValue);
+  }
+
+  public Long getPersonCalcValue() {
+    return personCalcValue;
+  }
+
+  public Long getCompanyCalcValue() {
+    return companyCalcValue;
   }
 }
--- src/test/java/br/com/helpdev/workshopunittest/business/FinanceSimulateBusinessTest.java
+++ src/test/java/br/com/helpdev/workshopunittest/business/FinanceSimulateBusinessTest.java
@@ -1,11 +1,21 @@
 package br.com.helpdev.workshopunittest.business;
 
+import br.com.helpdev.workshopunittest.business.model.SimulationData;
 import br.com.helpdev.workshopunittest.service.SerasaService;
+import br.com.helpdev.workshopunittest.service.objects.SerasaDebits;
+import java.util.Collections;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.util.ReflectionTestUtils;
 
+// - Why do you force the Spring Runner?. This becomes an integrated test
 @SpringBootTest
 class FinanceSimulateBusinessTest {
 
@@ -15,9 +25,34 @@ class FinanceSimulateBusinessTest {
   @Autowired
   private FinanceSimulateBusiness financeSimulateBusiness;
 
+  @BeforeEach
+  void setup() {
+    // - Don't use reflection.
+    // - Don't define a default values end behaviors here.
+
+    ReflectionTestUtils.setField(financeSimulateBusiness, "personCalcValue", 100L);
+    ReflectionTestUtils.setField(financeSimulateBusiness, "companyCalcValue", 20L);
+
+    Mockito.when(serasaService.getDebits(ArgumentMatchers.anyString())).thenReturn(Collections.emptyList());
+  }
+
+  // what is the value of this?
   @Test
-  void test() {
-    //TODO
+  void testValues() {
+    Assertions.assertEquals(100, financeSimulateBusiness.getPersonCalcValue());
+    Assertions.assertEquals(20L, financeSimulateBusiness.getCompanyCalcValue());
+  }
+
+  @Test
+  void testUserHasDebits() {
+    // - Don't need use stubs! use mocks
+    final var simulateData = new SimulationData("123", 10L, 10L, 10L);
+    final var debits = Collections.singletonList(new SerasaDebits(10L));
+
+    Mockito.when(serasaService.getDebits(simulateData.getDocument()))
+        .thenReturn(debits);
+
+    Assertions.assertThrows(SecurityException.class, () -> financeSimulateBusiness.simulate(simulateData));
   }
 
 }
