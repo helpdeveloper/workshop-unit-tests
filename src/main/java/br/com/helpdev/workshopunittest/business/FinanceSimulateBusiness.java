42c7cff91024b0aa7b03a11ab585dd47d41c3c59
Guilherme Biff Zarelli <guilherme.zarelli@luizalabs.com>
2021-05-05 16:52:38 -0300

    create test ex4

--- src/main/java/br/com/helpdev/workshopunittest/business/FinanceSimulateBusiness.java
+++ src/main/java/br/com/helpdev/workshopunittest/business/FinanceSimulateBusiness.java
@@ -1,8 +1,8 @@
 package br.com.helpdev.workshopunittest.business;
 
-import br.com.helpdev.workshopunittest.service.SerasaService;
 import br.com.helpdev.workshopunittest.business.model.SimulationData;
 import br.com.helpdev.workshopunittest.business.model.SimulationResult;
+import br.com.helpdev.workshopunittest.service.SerasaService;
 import br.com.helpdev.workshopunittest.utils.DocumentUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,26 +29,34 @@ public class FinanceSimulateBusiness {
 
     if (DocumentUtils.isCpf(simulationData.getDocument())) {
 
-      if ((simulationData.getFinancialValue() / simulationData.getIncome()) > personCalcValue) {
+      if (isAvailableToFinance(simulationData, personCalcValue)) {
         throw new IllegalArgumentException("You can't finance this value for person");
       }
 
     } else if (DocumentUtils.isCnpj(simulationData.getDocument())) {
 
-      if ((simulationData.getFinancialValue() / simulationData.getIncome()) > companyCalcValue) {
+      if (isAvailableToFinance(simulationData, companyCalcValue)) {
         throw new IllegalArgumentException("You can't finance this value for company");
       }
 
     }
 
-    return calculateParcel(simulationData);
+    return calculateParcelValue(simulationData);
   }
 
-  private SimulationResult calculateParcel(final SimulationData simulationData) {
+  // -- -- Private methods (Ex4)! I need test? How is the solution?  -- --
+
+  private SimulationResult calculateParcelValue(final SimulationData simulationData) {
     long parcelValue = simulationData.getFinancialValue() / simulationData.getParcels();
     parcelValue += parcelValue * 0.1f;
     return new SimulationResult(parcelValue);
   }
+
+  private boolean isAvailableToFinance(final SimulationData simulationData, final Long calcValue) {
+    return simulationData.getFinancialValue() / simulationData.getIncome() > calcValue;
+  }
+
+  // -- --
 
   public Long getPersonCalcValue() {
     return personCalcValue;
---
+++ src/test/java/br/com/helpdev/workshopunittest/business/Ex4_FinanceSimulateBusinessTest.java
@@ -0,0 +1,17 @@
+package br.com.helpdev.workshopunittest.business;
+
+import org.junit.jupiter.api.Test;
+
+class Ex4_FinanceSimulateBusinessTest {
+
+  @Test
+  void testPrivateMethod_calculateParcelValue() {
+    // TODO - How?
+  }
+
+  @Test
+  void testPrivateMethod_isAvailableToFinance() {
+    // TODO - How?
+  }
+
+}
